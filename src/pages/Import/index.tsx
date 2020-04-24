14dc237a77e56900bab770e12519c2a90d395bc1
Nilo Neregato <neregato.nilo@gmail.com>
2020-04-25 00:49:34 +0200

    upload files

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map(file => data.append(file.name, file.file));
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    console.log('files', files);
+
+    // eslint-disable-next-line no-underscore-dangle
+    const parsedFiles = files.map(
+      (file: File): FileProps => ({
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      }),
+    );
+    console.log('parsedFiles', parsedFiles);
+    setUploadedFiles(prevState => [...prevState, ...parsedFiles]);
+    console.log('uploadedFiles', uploadedFiles);
   }
 
   return (
